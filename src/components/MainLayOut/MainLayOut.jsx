abfc670292c41595841a8e7f2b64ab5a205bb5ac
Amimu10 <amimul211@gmail.com>
2023-10-09 16:38:37 +0600

    adjust container width

--- src/components/MainLayOut/MainLayOut.jsx
+++ src/components/MainLayOut/MainLayOut.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Header/Navbar/Navbar";
 
 const MainLayOut = () => {
     return (
-        <div className="max-w-[1140px] overflow-hidden px-4 mx-auto">       
+        <div className="max-w-[1440px] overflow-hidden px-4 mx-auto">       
             <Navbar></Navbar>
             <Outlet></Outlet> 
         </div>
